defceee38f930031ad2810afe2b9c7c472d7507d
Daniel Leidert <daniel.leidert@wgdd.de>
2019-11-11 15:49:59 +0100

    Add my current blog contents and the jekyll-import snippet

---
+++ tools/import_from_blogger.rb
@@ -0,0 +1,7 @@
+require "jekyll-import";
+
+JekyllImport::Importers::Blogger.run({
+  "source"                => File.expand_path('../blog-11-11-2019.xml', __FILE__),
+  "no-blogger-info"       => false, # not to leave blogger-URL info (id and old URL) in the front matter
+  "replace-internal-link" => false, # replace internal links using the post_url liquid tag.
+})
